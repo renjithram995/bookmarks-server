803ed9924966c13c34da1414b69263a7bf976ff8
rrenjith95 <rrenjith1995@gmail.com>
2024-08-28 13:26:59 +0530

    remove auth for health check added for testing

--- routes/healthCheck.js
+++ routes/healthCheck.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/authMiddleware');
 
-router.get('/', auth, (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json({
     timeZone: new Date().toISOString(),
     code: 200,
